a08964f24432de6417ed0f195b6e3ba40983ee65
Martin Kleinschrodt <martin@maklesoft.com>
2014-07-27 10:35:19 +0200

    Update overrides

---
+++ merges/android/config.js
@@ -0,0 +1,16 @@
+require.config({
+    baseUrl: "./",
+    paths: {
+        "padlock": "src",
+        "sjcl": "lib/sjcl",
+        // If the chrome storage is available, use that, otherwise use the localStorage api
+        "padlock/LocalSource": (typeof chrome !== "undefined") && chrome.storage ?
+            "src/ChromeStorageSource" : "src/LocalStorageSource"
+    },
+    shim: {
+        "sjcl": {
+            exports: "sjcl"
+        }
+    },
+    deps: ["cordova"]
+});
